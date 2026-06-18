b325b07120bec7005debca1fcf9be620d93c3556
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-18 20:14:25 +0530

    Update UserServiceImpl.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/UserServiceImpl.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/service/impl/UserServiceImpl.java
@@ -1,58 +1,37 @@
 package com.skills.hub.service.impl;
 
-import com.skills.hub.model.User;
+import com.skills.hub.entity.User;
 import com.skills.hub.repository.UserRepository;
 import com.skills.hub.service.UserService;
 import org.springframework.stereotype.Service;
 
-/*
-=========================================================
-WHAT IS THIS CLASS?
-=========================================================
-
-This class contains BUSINESS LOGIC for users.
-
- Controller calls this
- This class talks to repository
-
-=========================================================
-*/
-
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepo;
+    private final UserRepository repo;
 
-    public UserServiceImpl(UserRepository userRepo) {
-        this.userRepo = userRepo;
+    public UserServiceImpl(UserRepository repo) {
+        this.repo = repo;
     }
 
     @Override
     public User registerUser(User user) {
-
-        // =========================
-        // TASK
-        // =========================
-        // STEP 1: check if email already exists
-        // STEP 2: if exists → stop process
-        // STEP 3: if not → save user to DB
-        // STEP 4: return saved user
-
-        return null;
+        return repo.save(user);
     }
 
     @Override
     public User login(String email, String password) {
+        User user = repo.findByEmail(email);
 
-        // =========================
-        // TASK
-        // =========================
-        // STEP 1: find user by email
-        // STEP 2: if user not found → return null
-        // STEP 3: check password match
-        // STEP 4: if correct → return user
-        // STEP 5: else → return null
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
 
         return null;
     }
 }
+
+
+
+
+userserviceimpl
