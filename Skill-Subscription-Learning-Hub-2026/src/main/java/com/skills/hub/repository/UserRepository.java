f954dccad4bbcb4db35c638d9d07473614557e89
imca24mimd28-eng <imca.24mimd28@silicon.ac.in>
2026-06-18 20:10:32 +0530

    Update UserRepository.java

--- Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/repository/UserRepository.java
+++ Skill-Subscription-Learning-Hub-2026/src/main/java/com/skills/hub/repository/UserRepository.java
@@ -1,32 +1,8 @@
 package com.skills.hub.repository;
 
-import com.skills.hub.model.User;
+import com.skills.hub.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*
-=========================================================
-WHAT IS THIS FILE?
-=========================================================
-
-UserRepository is used to communicate with the database
-for USER-related operations.
-
- It automatically handles:
-- Save user
-- Find user
-- Delete user
-- Update user
-
-=========================================================
-
-SIMPLE MEANING:
-No SQL needed — Spring handles it automatically
-=========================================================
-*/
-
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    // Custom query method
-    // Used for login validation
     User findByEmail(String email);
 }
